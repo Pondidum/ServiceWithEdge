5050d43dc47790b3e594879f1f950cced0d3c512
andy <andy@andydote.co.uk>
2014-08-10 18:45:41 +0100

    service: use dash

--- ServiceWithEdge/Program.cs
+++ ServiceWithEdge/Program.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.IO;
-using System.Reflection;
-using System.Threading;
-using System.Threading.Tasks;
-using EdgeJs;
-using ServiceWithEdge.Models;
+using Dash;
 
 namespace ServiceWithEdge
 {
@@ -12,78 +7,23 @@ namespace ServiceWithEdge
 	{
 		static void Main(string[] args)
 		{
-			try
-			{
-
-				var store = new ModelStore();
-
-				RunSomeService(store);
-				RunWebui(store);
-
-			}
-			catch (Exception ex)
-			{
-				Console.Write(ex);
-			}
-
+			RunWebui();
 			Console.ReadKey();
 		}
 
-		static void RunSomeService(ModelStore store)
+		private static void RunWebui()
 		{
-			Task.Run(() =>
-			{
-				var model = new IndexModel();
-				store.Register(model);
+			var ui = new DashWebUI();
+			ui.Start();
+			//var router = new RouterGenerator(store);
 
-				while (true)
-				{
-					Thread.Sleep(500);
-					model.Iterations++;
-				}
-			});
-		}
+			//var app = GetApp().Replace("//{Models}", router.Generate());
+			//var func = Edge.Func(app);
 
-		private static string GetApp()
-		{
-			var asm = Assembly.GetExecutingAssembly();
-			var name = "ServiceWithEdge.webui.app.js";
-
-			using (var s = asm.GetManifestResourceStream(name))
-			using (var reader = new StreamReader(s))
-			{
-				return reader.ReadToEnd();
-			}
-		}
-
-		private static void RunWebui(ModelStore store)
-		{
-			var router = new RouterGenerator(store);
-
-			var app = GetApp().Replace("//{Models}", router.Generate());
-			var func = Edge.Func(app);
-
-			var getModel = (Func<object, Task<object>>)(async (message) =>
-			{
-				return store.GetModel((string)message);
-			});
-
-
-			Task.Run(() =>
-			{
-				try
-				{
-					func(new
-					{
-						getModel,
-						port = 3000,
-					});
-				}
-				catch (Exception ex)
-				{
-					Console.WriteLine(ex.ToString());
-				}
-			});
+			//var getModel = (Func<object, Task<object>>)(async (message) =>
+			//{
+			//	return store.GetModel((string)message);
+			//});
 		}
 	}
 }
